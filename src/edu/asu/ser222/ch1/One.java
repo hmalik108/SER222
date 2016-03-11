c152b8735df1da3da8d1bc4fa1978d0ae0d80e9a
Harish Malik <hmalik2@asu.edu>
2016-03-11 04:05:19 -0700

    Hello World

---
+++ src/edu/asu/ser222/ch1/One.java
@@ -0,0 +1,11 @@
+package edu.asu.ser222.ch1;
+
+/**
+ * Created by HM on 3/11/16.
+ */
+public class One {
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+    }
+}
